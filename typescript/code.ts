979f97a6fc04707bfdcd0385670332942cbe7579
Evgeny Lyozin <evgenylyozindev@gmail.com>
2022-12-29 09:04:08 +0300

    ts check skip for a single line

--- typescript/code.ts
+++ typescript/code.ts
@@ -1,5 +1,6 @@
 /**
  * To run in vscode:
  * ctrl+shift+d -> select TS Launch -> f5
- * if you want to skip ts ckecks for some reason - // @ts-nocheck
+ * if you want to skip ts ckecks for the entire file - // @ts-nocheck
+ * if you want to skip ts ckecks for a single line - // @ts-ignore
  */
