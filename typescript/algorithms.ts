5744747802241577c7f0421b268757fda13cd100
Evgeny Lyozin <evgenylyozindev@gmail.com>
2023-01-31 19:26:58 +0300

    meta binary search for ts

--- typescript/algorithms.ts
+++ typescript/algorithms.ts
@@ -41,3 +41,25 @@ const BinarySearchRecursive = (
 
 // console.log(BinarySearchRecursive([1, 2, 3, 4, 5, 6, 7, 8, 9], 3, 0, 8)); //=> 2
 // console.log(BinarySearchRecursive([1, 2, 3, 4, 5, 6, 7, 8, 9], 10, 0, 8)); //=> -1
+
+// Meta binary search (one sided binary search)
+const BinarySearchMeta = (arr: number[], whatToSearchFor: number) => {
+  let bitsToHoldArrLength = 0;
+  while (1 << bitsToHoldArrLength < arr.length - 1) {
+    bitsToHoldArrLength++;
+  }
+  let pos = 0;
+
+  for (let i = bitsToHoldArrLength; i >= 0; i--) {
+    if (arr[pos] === whatToSearchFor) return pos;
+    const newPos = pos | (1 << i);
+
+    if (newPos < arr.length && arr[newPos] <= whatToSearchFor) {
+      pos = newPos;
+    }
+  }
+  return arr[pos] === whatToSearchFor ? pos : -1;
+};
+
+// console.log(BinarySearchMeta([1, 2, 3, 4, 5, 6, 7, 8, 9], 3)); //=> 2
+// console.log(BinarySearchMeta([1, 2, 3, 4, 5, 6, 7, 8, 9], 10)); //=> -1
