2877d18aa459cc1452cb86deee2395834f7f7699
Evgeny Lyozin <evgenylyozindev@gmail.com>
2023-02-01 10:18:27 +0300

    ternary search for go

--- go/algorithms/algorithms.go
+++ go/algorithms/algorithms.go
@@ -87,3 +87,81 @@ func BinarySearchMeta(arr []int, whatToSearchFor int) int {
 // 	fmt.Println(algs.BinarySearchMeta([]int{1, 2, 3, 4, 5, 6, 7, 8, 9}, 3))  // => 2
 // 	fmt.Println(algs.BinarySearchMeta([]int{1, 2, 3, 4, 5, 6, 7, 8, 9}, 10)) // => -1
 // }
+
+// TernarySearchIterative - iterative version of ternary search
+func TernarySearchIterative(arr []int, whatToSearchFor int) int {
+	left := 0
+	right := len(arr) - 1
+
+	for left <= right {
+		mid1 := left + (right-left)/3
+		mid2 := right - (right-left)/3
+
+		if arr[mid1] == whatToSearchFor {
+			return mid1
+		}
+
+		if arr[mid2] == whatToSearchFor {
+			return mid2
+		}
+
+		if arr[mid1] > whatToSearchFor {
+			right = mid1 - 1
+			continue
+		}
+		if arr[mid2] < whatToSearchFor {
+			left = mid2 + 1
+			continue
+		}
+		left = mid1 + 1
+		right = mid2 - 1
+	}
+	return -1
+}
+
+// import (
+// 	"fmt"
+// 	algs "leetcode/algorithms"
+// )
+
+// func main() {
+// 	fmt.Println(algs.TernarySearchIterative([]int{1, 2, 3, 4, 5, 6, 7, 8, 9}, 3))  // => 2
+// 	fmt.Println(algs.TernarySearchIterative([]int{1, 2, 3, 4, 5, 6, 7, 8, 9}, 10)) // => -1
+// }
+
+// TernarySearchRecursive - recursive version of ternary search
+func TernarySearchRecursive(arr []int, whatToSearchFor, left, right int) int {
+	if left > right {
+		return -1
+	}
+
+	mid1 := left + (right-left)/3
+	mid2 := right - (right-left)/3
+
+	if arr[mid1] == whatToSearchFor {
+		return mid1
+	}
+	if arr[mid2] == whatToSearchFor {
+		return mid2
+	}
+
+	if arr[mid1] > whatToSearchFor {
+		return TernarySearchRecursive(arr, whatToSearchFor, left, mid1-1)
+	}
+
+	if arr[mid2] < whatToSearchFor {
+		return TernarySearchRecursive(arr, whatToSearchFor, mid2+1, right)
+	}
+
+	return TernarySearchRecursive(arr, whatToSearchFor, mid1+1, mid2-1)
+}
+
+// import (
+// 	"fmt"
+// 	algs "leetcode/algorithms"
+// )
+
+// func main() {
+// 	fmt.Println(algs.TernarySearchRecursive([]int{1, 2, 3, 4, 5, 6, 7, 8, 9}, 3,0,8))  // => 2
+// 	fmt.Println(algs.TernarySearchRecursive([]int{1, 2, 3, 4, 5, 6, 7, 8, 9}, 10,0,8)) // => -1
+// }
